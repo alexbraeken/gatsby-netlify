efae188f0bcf272eac855e05b9dd1609202949cc
Alex <alex_barca2006@hotmail.com>
2020-10-25 23:39:28 +0000

    needs more fixes :(

--- gatsby-starter-netlify-cms/src/components/PropFeatures.js
+++ gatsby-starter-netlify-cms/src/components/PropFeatures.js
@@ -10,61 +10,53 @@ const PropFeatureGrid = (data) => {
   /*const [filteredSearch, setFilteredSearch] = useState()*/
 
 
+
   useEffect(() => {
+
     console.log(data.state.city);
+    console.log(data.state.propType);
+    console.log(data.state.bathrooms);
+    console.log(data.state.bedrooms);
+    console.log(data.state.filteredSearch);
     
     const list = data.gridItems.value.map((item, index) => {
-      console.log("new prop: " +index)
-      console.log(data.state.city.includes(item.city) === data.state.filteredSearch)
-      console.log("end prop")
-      if((data.state.city.includes(item.city) === data.state.filteredSearch)
+      //if(data.state.city.includes(item.city) === data.state.filteredSearch)
+
+      /*if((data.state.city.includes(item.city) === data.state.filteredSearch)
       && (data.state.propType.indexOf(item.type) === -1)
       && (data.state.bedrooms[0] <= parseInt(item.bedrooms))
       && (parseInt(item.bedrooms) <= data.state.bedrooms[1])
       && (data.state.bathrooms[0] <= parseInt(item.bathrooms)) 
-      && (parseInt(item.bathrooms) <= data.state.bathrooms[1])){
-        console.log("found " + index)
+      && (parseInt(item.bathrooms) <= data.state.bathrooms[1])){*/
         return item
-    }
+    //}
   })
 
-  setPropList(list)
-
-    return () => {
-      setPropList([])
-    }
-  }, [data.state.city, data.state.propType, data.state.bathrooms, data.state.bedrooms, data.state.filteredSearch])
-
-
-
-  useEffect(() => {
-
-    const list = propList;
-
     if(data.sort === "price-min"){
-      list.sort((a, b)=>(a.item.baseDailyRate > b.item.baseDailyRate) ? 1 : ((b.item.baseDailyRate > a.item.baseDailyRate) ? -1 : 0));
+      list.sort((a, b)=>(a.baseDailyRate > b.baseDailyRate) ? 1 : ((b.baseDailyRate > a.baseDailyRate) ? -1 : 0));
     }
     if(data.sort === "price-max"){
-      list.sort((a, b)=>(a.item.baseDailyRate < b.item.baseDailyRate) ? 1 : ((b.item.baseDailyRate < a.item.baseDailyRate) ? -1 : 0));
+      list.sort((a, b)=>(a.baseDailyRate < b.baseDailyRate) ? 1 : ((b.baseDailyRate < a.baseDailyRate) ? -1 : 0));
     }
     if(data.sort === "bedrooms-min"){
-      list.sort((a, b)=>(a.item.bedrooms > b.item.bedrooms) ? 1 : ((b.item.bedrooms > a.item.bedrooms) ? -1 : 0));
+      list.sort((a, b)=>(a.bedrooms > b.bedrooms) ? 1 : ((b.bedrooms > a.bedrooms) ? -1 : 0));
     }
     if(data.sort === "bedrooms-max"){
-      list.sort((a, b)=>(a.item.bedrooms < b.item.bedrooms) ? 1 : ((b.item.bedrooms < a.item.bedrooms) ? -1 : 0));
+      list.sort((a, b)=>(a.bedrooms < b.bedrooms) ? 1 : ((b.bedrooms < a.bedrooms) ? -1 : 0));
     }
 
     setPropList(list)
 
+    console.log(propList.length)
     return () => {
-      setPropList([])
+      //setPropList([])
     }
-  }, [data.sort, data.state.filteredSearch])
+  }, [data])
 
 
   return(
   <div className="columns is-multiline" style={{margin:"auto", justifyContent:"center"}}>
-    {propList.map((item, index) => 
+    {propList.length> 0 && propList.map((item, index) => 
     {
       return(
       <PropertyCard item={item} data={data} index={index} key={index} handleGalleryClick={data.handleGalleryClick}/>)}
