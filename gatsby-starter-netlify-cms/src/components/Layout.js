9c1508c7c741c9c53e162b6bb7be54c1082695c5
Alex <alex_barca2006@hotmail.com>
2021-03-15 03:45:39 +0000

    moved mailerlite script to component

--- gatsby-starter-netlify-cms/src/components/Layout.js
+++ gatsby-starter-netlify-cms/src/components/Layout.js
@@ -66,16 +66,6 @@ const TemplateWrapper = ({ children }) => {
 
 <script type="text/javascript" src="https://platform.hostfully.com/assets/js/leadCaptureWidget_2.0.js"/>
         <script type="text/javascript" src="https://platform.hostfully.com/assets/widgets/searchwidget/searchwidget.js" />
-<script>{`(function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
-var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
-f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
-var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
-_.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
-
-var ml_account = ml('accounts', '2710252', 's7t2o9x9p0', 'load');`}
-</script>
-
-
     
       </Helmet>
       <FirestoreProvider {...config} firebase={firebase}>
--- gatsby-starter-netlify-cms/src/components/Newsletter.js
+++ gatsby-starter-netlify-cms/src/components/Newsletter.js
@@ -5,6 +5,14 @@ import { Helmet } from 'react-helmet'
 const Newsletter = () => (
         <Container style={{margin:"50px auto"}}>
             <Helmet>
+            <script>{`(function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
+var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
+f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
+var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
+_.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+
+var ml_account = ml('accounts', '2710252', 's7t2o9x9p0', 'load');`}
+</script>
             <style>
                 {`
                 #subscribe-form #mlb2-3228322.ml-form-embedContainer .ml-form-embedWrapper.embedForm{
@@ -15,6 +23,7 @@ const Newsletter = () => (
                     max-width:400px
                 }
                 }`}
+                
             </style>
             </Helmet>
             <div className="ml-form-embed"
