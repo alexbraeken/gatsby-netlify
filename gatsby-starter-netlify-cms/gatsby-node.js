b3599c07cf6e6e5e167cbc1f8d48f131fbe860b4
Alex <alex_barca2006@hotmail.com>
2021-05-19 11:19:21 +0100

    rebuilt with property nodes

--- gatsby-starter-netlify-cms/gatsby-node.js
+++ gatsby-starter-netlify-cms/gatsby-node.js
@@ -124,16 +124,14 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
   fmImagesToRelative(node) // convert image paths for gatsby images
 
-  /*if (node.internal.type === `MarkdownRemark`) {
+  if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
-    console.log(node)
-    console.log("value: " + value)
     createNodeField({
       name: `slug`,
       node,
       value,
     })
-  }*/
+  }
 }
 
 exports.onCreateWebpackConfig = ({
